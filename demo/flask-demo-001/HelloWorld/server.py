1b250d368ae4bebc952b7048571a5ca1920f8789
wuhaijie <wuhaijie@outlook.com>
2019-10-04 11:42:03 +0800

    Show sample to Ray, commit by git commandline

--- demo/flask-demo-001/HelloWorld/server.py
+++ demo/flask-demo-001/HelloWorld/server.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#1 line comment
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000)# debug=true can not run
